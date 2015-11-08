a95490d9e19f4783eb26b4bc520a58ed657b6c34
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2015-11-07 18:16:39 -0800

    qubit class is ready to go

--- qubit.py
+++ qubit.py
@@ -4,18 +4,32 @@ import math
 class Qubit:
 
    name = ""
-   v0 = 0
-   v1 = 1
+   v_0 = 0
+   v_1 = 1
+   final_state = 0
+   state_read = False
 
    def __init__(self, name): 
       self.name = name
-      self.randomizeState()
-      print name
+      self.randomize_state()
 
-   def randomizeState(self):
+   def randomize_state(self):
       rand = random.random()
-      randComp = 1 - rand
-      v0 = math.sqrt(rand)
-      v1 = math.sqrt(randComp)
-      print v0
-      print v1
+      rand_comp = 1 - rand
+      self.v_0 = math.sqrt(rand)
+      self.v_1 = math.sqrt(rand_comp)
+
+   def read_state(self):
+      if not(self.state_read):
+
+         self.state_read = True
+         rand = random.random()
+
+         if rand > self.v_0:
+            self.final_state = 1
+         elif rand < self.v_0:
+            self.final_state = 0
+         else:
+            self.read_state()
+
+      return self.final_state
